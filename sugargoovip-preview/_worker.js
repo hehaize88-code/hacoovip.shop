9f3d27ced29e42791f41190d84031b2ea7a7194a
hehaize88-code <hehaize88@gmail.com>
2026-08-10 14:51:39 +0800

    Serve the upgraded UK homepage and sitemap shell directly

--- sugargoovip-preview/_worker.js
+++ sugargoovip-preview/_worker.js
@@ -2,7 +2,19 @@ import core from './worker-core-uk-20260808.js';
 
 export default {
   async fetch(request, env, ctx) {
-    const pathname = new URL(request.url).pathname;
+    const url = new URL(request.url);
+    const pathname = url.pathname;
+
+    if (pathname === '/' || pathname === '/index.html') {
+      const assetUrl = new URL(request.url);
+      assetUrl.pathname = '/index.html';
+      return env.ASSETS.fetch(new Request(assetUrl.toString(), request));
+    }
+
+    if (pathname === '/robots.txt' || pathname === '/sitemap-index.xml') {
+      return env.ASSETS.fetch(request);
+    }
+
     if (
       pathname.startsWith('/assets/') ||
       pathname.startsWith('/tools/') ||
@@ -10,6 +22,7 @@ export default {
     ) {
       return env.ASSETS.fetch(request);
     }
+
     return core.fetch(request, env, ctx);
   }
 };
