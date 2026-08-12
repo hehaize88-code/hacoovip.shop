c9e6e3f7d796c6d72cf7b479f26f912fa4a9cf4a
hehaize88-code <hehaize88@gmail.com>
2026-08-12 16:43:12 +0800

    Fix AllChinaBuy Cloudflare Pages runtime

---
+++ allchinabuy-ro/_worker.js/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,48 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-FRIYiNmt.js\")",
+  "clientReferenceDeps": {
+    "593f344dc510": {
+      "js": [
+        "/assets/index-FRIYiNmt.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-B_IAiODp.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-FRIYiNmt.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "c2747888630f": {
+      "js": [
+        "/assets/link-BafQgLQa.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-FRIYiNmt.js",
+        "/assets/framework-XU8gKkFC.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-FRIYiNmt.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-C2PdpzJg.css"
+      ]
+    }
+  }
+}
