d54eea029c78b862166535c446a81969b8cb4579
hehaize88-code <hehaize88@gmail.com>
2026-08-12 23:40:30 +0800

    Fix superbuys.id sitemap crawling

--- superbuys-id/app/robots.ts
+++ superbuys-id/app/robots.ts
@@ -3,5 +3,11 @@ import type { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://superbuys.id/sitemap.xml" };
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: [
+      "https://superbuys.id/google-sitemap.xml",
+      "https://superbuys.id/sitemap.xml",
+    ],
+  };
 }
