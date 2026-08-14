6bec3cc91a4d6780f9e70db4d3c0ba67f0eae909
hehaize88-code <hehaize88@gmail.com>
2026-08-14 18:23:16 +0800

    Make Sheet Hipobuy SEO routes static

--- sheet-hipobuy-net/app/robots.ts
+++ sheet-hipobuy-net/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const origin = "https://sheet-hipobuy.net";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
